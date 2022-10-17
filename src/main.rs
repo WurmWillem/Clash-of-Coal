3915b5ba8e0ed4f0a7074c096907f17082399e56
WurmWillem <mcthije@gmail.com>
2022-10-17 18:45:38 +0200

    Added map with texture

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,35 @@ use macroquad::prelude::*;
 pub const SCREENSIZE: (f32, f32) = (720., 720.); //Change this if you want to, old was 640, 480
 pub const CAMERA_MOVE_MULT: f32 = 2.;
 
+struct Map {
+    tex: Texture2D,
+}
+impl Map {
+    pub fn new(tex: Texture2D) -> Self{
+        Self { tex, }
+    }
+
+    pub fn draw(&self) {
+        let params = DrawTextureParams {
+            dest_size: Some(Vec2::new(1., 1.)),
+            ..Default::default()
+        };
+
+        draw_texture_ex(self.tex, 0., 0., WHITE, params.clone());
+    }
+}
+
 #[macroquad::main("Clash of clans")]
 async fn main() {
     request_new_screen_size(SCREENSIZE.0, SCREENSIZE.1); //Set new screensize
 
+    let map_tex = load_texture("map.png").await.unwrap();
+    let map = Map::new(map_tex);
+
     // Vars
     let mut camera_origin = (0., 0.);
     let mut offset = (0., 0.);
-
+    
     // Game loop
     loop {
         // Clear screen
@@ -23,10 +44,10 @@ async fn main() {
             camera_origin.0 += 0.01;   //update where the camera looks at
         };
         if is_key_down(KeyCode::S) {
-            camera_origin.1 += 0.01;   //update where the camera looks at
+            camera_origin.1 -= 0.01;   //update where the camera looks at
         };
         if is_key_down(KeyCode::W) {
-            camera_origin.1 -= 0.01;   //update where the camera looks at
+            camera_origin.1 += 0.01;   //update where the camera looks at
         };
 
         // Update map position
@@ -45,8 +66,8 @@ async fn main() {
             target: vec2(camera_origin.0  * -CAMERA_MOVE_MULT, camera_origin.1 * CAMERA_MOVE_MULT),
             ..Default::default()
         });
-
-        draw_rectangle(0., 0., 0.2, 0.2, GREEN);
+        
+        map.draw();
 
         // Next_frame
         next_frame().await;

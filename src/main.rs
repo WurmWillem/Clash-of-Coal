48d97d1dd80a98c67177f2ff6ad319f4acf4b1e4
WurmWillem <mcthije@gmail.com>
2022-10-17 18:21:50 +0200

    Refactored to work with Camera2D

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,52 @@
 use macroquad::prelude::*;
 
-// Map
-struct Map {
-    width: f32,
-    height: f32,
-}
+pub const SCREENSIZE: (f32, f32) = (720., 720.); //Change this if you want to, old was 640, 480
+pub const CAMERA_MOVE_MULT: f32 = 2.;
 
-struct Object {
-    x: f32,
-    y: f32,
-}
-
-// Clash of clans
 #[macroquad::main("Clash of clans")]
-
-// Main
 async fn main() {
-    // Map struct
-    let map = Map {
-        width: 300.0,
-        height: 300.0,
-    };
-
-    // Object struct
-    let house = Object {
-        // Coords relative to map
-        x: 100.,
-        y: 100.,
-    };
+    request_new_screen_size(SCREENSIZE.0, SCREENSIZE.1); //Set new screensize
 
     // Vars
-    let mut camera_origin = (400., 300.);
+    let mut camera_origin = (0., 0.);
     let mut offset = (0., 0.);
 
     // Game loop
     loop {
         // Clear screen
         clear_background(BLACK);
+        
+        if is_key_down(KeyCode::A) {
+            camera_origin.0 -= 0.01;   //update where the camera looks at
+        };
+        if is_key_down(KeyCode::D) {
+            camera_origin.0 += 0.01;   //update where the camera looks at
+        };
+        if is_key_down(KeyCode::S) {
+            camera_origin.1 += 0.01;   //update where the camera looks at
+        };
+        if is_key_down(KeyCode::W) {
+            camera_origin.1 -= 0.01;   //update where the camera looks at
+        };
 
         // Update map position
         if is_mouse_button_pressed(MouseButton::Left) {
-            offset = (mouse_position().0 - camera_origin.0, mouse_position().1 - camera_origin.1);
+            offset = (
+                mouse_position().0 / SCREENSIZE.0 - camera_origin.0,
+                mouse_position().1 / SCREENSIZE.1 - camera_origin.1,
+            );
         }
         if is_mouse_button_down(MouseButton::Left) {
-            camera_origin = (mouse_position().0 - offset.0, mouse_position().1 - offset.1);
+            camera_origin.0 = mouse_position().0 / SCREENSIZE.0 - offset.0; 
+            camera_origin.1 = mouse_position().1 / SCREENSIZE.1 - offset.1; 
         }
 
-        // Draw map
-        draw_rectangle(camera_origin.0 - map.width / 2., camera_origin.1 - map.height / 2., map.width, map.height, WHITE);
-        
-        // Make a loop for this-------------
-        draw_rectangle(camera_origin.0 - map.width / 2. + 0., camera_origin.1 - map.height / 2., 50., 50., GREEN);
-        draw_rectangle(camera_origin.0 - map.width / 2. + 50., camera_origin.1 - map.height / 2., 50., 50., DARKGREEN);
-        draw_rectangle(camera_origin.0 - map.width / 2. + 100., camera_origin.1 - map.height / 2., 50., 50., GREEN);
-        draw_rectangle(camera_origin.0 - map.width / 2. + 150., camera_origin.1 - map.height / 2., 50., 50., DARKGREEN);
-        draw_rectangle(camera_origin.0 - map.width / 2. + 200., camera_origin.1 - map.height / 2., 50., 50., GREEN);
-        draw_rectangle(camera_origin.0 - map.width / 2. + 250., camera_origin.1 - map.height / 2., 50., 50., DARKGREEN);
+        set_camera(&Camera2D {
+            target: vec2(camera_origin.0  * -CAMERA_MOVE_MULT, camera_origin.1 * CAMERA_MOVE_MULT),
+            ..Default::default()
+        });
 
-        draw_rectangle(camera_origin.0 - map.width / 2. + 0., camera_origin.1 - map.height / 2. + 50., 50., 50., DARKGREEN);
-        draw_rectangle(camera_origin.0 - map.width / 2. + 50., camera_origin.1 - map.height / 2. + 50., 50., 50., GREEN);
-        draw_rectangle(camera_origin.0 - map.width / 2. + 100., camera_origin.1 - map.height / 2. + 50., 50., 50., DARKGREEN);
-        draw_rectangle(camera_origin.0 - map.width / 2. + 150., camera_origin.1 - map.height / 2. + 50., 50., 50., GREEN);
-        draw_rectangle(camera_origin.0 - map.width / 2. + 200., camera_origin.1 - map.height / 2. + 50., 50., 50., DARKGREEN);
-        draw_rectangle(camera_origin.0 - map.width / 2. + 250., camera_origin.1 - map.height / 2. + 50., 50., 50., GREEN);
-
-        // Draw house on map (black square)
-        draw_rectangle(camera_origin.0 - map.width / 2. + house.x, camera_origin.1 - map.height / 2. + house.y, 10., 10., BLACK);
+        draw_rectangle(0., 0., 0.2, 0.2, GREEN);
 
         // Next_frame
         next_frame().await;

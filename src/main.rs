2c17f30214dd7dbabdb85027429d9c472a2c44ef
WurmWillem <mcthije@gmail.com>
2022-10-17 18:50:59 +0200

    Made camera into its own struct

--- src/main.rs
+++ src/main.rs
@@ -21,55 +21,62 @@ impl Map {
     }
 }
 
-#[macroquad::main("Clash of clans")]
-async fn main() {
-    request_new_screen_size(SCREENSIZE.0, SCREENSIZE.1); //Set new screensize
-
-    let map_tex = load_texture("map.png").await.unwrap();
-    let map = Map::new(map_tex);
-
-    // Vars
-    let mut camera_origin = (0., 0.);
-    let mut offset = (0., 0.);
-    
-    // Game loop
-    loop {
-        // Clear screen
-        clear_background(BLACK);
-        
+struct Camera {
+    origin: (f32, f32),
+    offset: (f32, f32),
+}
+impl Camera {
+    fn new() -> Self {
+        Self { origin: (0., 0.), offset: (0., 0.) }
+    }
+    pub fn update(&mut self) {
         if is_key_down(KeyCode::A) {
-            camera_origin.0 -= 0.01;   //update where the camera looks at
+            self.origin.0 -= 0.01;   //update where the camera looks at
         };
         if is_key_down(KeyCode::D) {
-            camera_origin.0 += 0.01;   //update where the camera looks at
+            self.origin.0 += 0.01;   //update where the camera looks at
         };
         if is_key_down(KeyCode::S) {
-            camera_origin.1 -= 0.01;   //update where the camera looks at
+            self.origin.1 -= 0.01;   //update where the camera looks at
         };
         if is_key_down(KeyCode::W) {
-            camera_origin.1 += 0.01;   //update where the camera looks at
+            self.origin.1 += 0.01;   //update where the camera looks at
         };
 
         // Update map position
         if is_mouse_button_pressed(MouseButton::Left) {
-            offset = (
-                mouse_position().0 / SCREENSIZE.0 - camera_origin.0,
-                mouse_position().1 / SCREENSIZE.1 - camera_origin.1,
+            self.offset = (
+                mouse_position().0 / SCREENSIZE.0 - self.origin.0,
+                mouse_position().1 / SCREENSIZE.1 - self.origin.1,
             );
         }
         if is_mouse_button_down(MouseButton::Left) {
-            camera_origin.0 = mouse_position().0 / SCREENSIZE.0 - offset.0; 
-            camera_origin.1 = mouse_position().1 / SCREENSIZE.1 - offset.1; 
+            self.origin.0 = mouse_position().0 / SCREENSIZE.0 - self.offset.0; 
+            self.origin.1 = mouse_position().1 / SCREENSIZE.1 - self.offset.1; 
         }
 
         set_camera(&Camera2D {
-            target: vec2(camera_origin.0  * -CAMERA_MOVE_MULT, camera_origin.1 * CAMERA_MOVE_MULT),
+            target: vec2(self.origin.0  * -CAMERA_MOVE_MULT, self.origin.1 * CAMERA_MOVE_MULT),
             ..Default::default()
         });
+    }
+}
+
+#[macroquad::main("Clash of clans")]
+async fn main() {
+    request_new_screen_size(SCREENSIZE.0, SCREENSIZE.1); //Set new screensize
+
+    //Initialize map and camera
+    let map_tex = load_texture("map.png").await.unwrap();
+    let map = Map::new(map_tex);
+    let mut camera = Camera::new();
+    
+    loop {
+        clear_background(BLACK);
         
+        camera.update();
         map.draw();
 
-        // Next_frame
         next_frame().await;
     }
 }
